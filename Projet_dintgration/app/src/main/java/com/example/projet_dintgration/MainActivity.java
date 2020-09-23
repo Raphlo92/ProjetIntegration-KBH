7e2b6245ac5fd12a4174ff2b32067896fa264714
Mobius2000 <45171607+Mobius2000@users.noreply.github.com>
2020-09-23 17:22:58 -0400

    sa marche
    sa marche bien

--- Projet_dintgration/app/src/main/java/com/example/projet_dintgration/MainActivity.java
+++ Projet_dintgration/app/src/main/java/com/example/projet_dintgration/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.projet_dintgration;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMusicPage();
+            }
+        });
+    }
+    public void openMusicPage(){
+        Intent intent = new Intent(this, Music_Page.class);
+        startActivity(intent);
     }
 }
---
+++ Projet_dintgration/app/src/main/java/com/example/projet_dintgration/Music_Page.java
@@ -0,0 +1,17 @@
+package com.example.projet_dintgration;
+
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class Music_Page extends AppCompatActivity {
+    MediaPlayer mediaPlayer;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_music__page);
+
+    }
+}
