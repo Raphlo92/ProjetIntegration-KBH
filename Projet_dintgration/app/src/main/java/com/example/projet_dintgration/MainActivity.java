8e15a10ce417109ae4cba7cf2fd01a99a231c167
Mobius2000 <45171607+Mobius2000@users.noreply.github.com>
2020-09-30 13:55:03 -0400

    ok
    ok

--- Projet_dintgration/app/src/main/java/com/example/projet_dintgration/MainActivity.java
+++ Projet_dintgration/app/src/main/java/com/example/projet_dintgration/MainActivity.java
@@ -7,13 +7,23 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
     private Button button;
+    private static int SPLASH_TIME_OUT = 400;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+                 Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                 startActivity(homeIntent);
+                 finish();
+            }
+        },SPLASH_TIME_OUT);
 
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
