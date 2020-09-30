f43b6dd618a8cbadcab69046c57765545a265b55
Mobius2000 <45171607+Mobius2000@users.noreply.github.com>
2020-09-29 20:22:18 -0400

    sa marche
    sa marche bien

--- Projet_dintgration/app/src/main/java/com/example/projet_dintgration/Music_Page.java
+++ Projet_dintgration/app/src/main/java/com/example/projet_dintgration/Music_Page.java
@@ -2,6 +2,8 @@ package com.example.projet_dintgration;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,5 +15,42 @@ public class Music_Page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music__page);
 
+    }
+
+    public void play(View v){
+        if (mediaPlayer == null) {
+            mediaPlayer = MediaPlayer.create(this, R.raw.boku);
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopplayer();
+                }
+            });
+        }
+        mediaPlayer.start();
+    }
+
+    public void pause(View v){
+        if (mediaPlayer != null) {
+            mediaPlayer.pause();
+        }
+    }
+
+    public void stop(View v)
+    {
+        stopplayer();
+    }
+
+    private void stopplayer() {
+        if (mediaPlayer != null) {
+            mediaPlayer.release();
+            mediaPlayer = null;
+            Toast.makeText(this, "mediaPlayer released", Toast.LENGTH_LONG).show();
+        }
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        stopplayer();
     }
 }
