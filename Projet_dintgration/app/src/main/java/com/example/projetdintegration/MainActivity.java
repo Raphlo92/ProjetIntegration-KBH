9017495450d05a5708c1ce4791d93805a778e0c9
Wazarel_Namara <55587042+Raphlo92@users.noreply.github.com>
2020-10-14 20:26:46 -0400

    yeet

--- Projet_dintgration/app/src/main/java/com/example/projetdintegration/MainActivity.java
+++ Projet_dintgration/app/src/main/java/com/example/projetdintegration/MainActivity.java
@@ -14,6 +14,13 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
