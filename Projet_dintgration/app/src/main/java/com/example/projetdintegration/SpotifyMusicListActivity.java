26973fe18e6e96c4ab3cf3e4e9b55189f5747418
Covoclock <58307962+Covoclock@users.noreply.github.com>
2020-10-21 17:51:30 -0400

    Endless scroll added + get all children of Spotify Elements -- Yeah !

--- Projet_dintgration/app/src/main/java/com/example/projetdintegration/SpotifyMusicListActivity.java
+++ Projet_dintgration/app/src/main/java/com/example/projetdintegration/SpotifyMusicListActivity.java
@@ -2,19 +2,26 @@ package com.example.projetdintegration;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.viewpager2.widget.ViewPager2;
 
 import com.google.android.material.navigation.NavigationView;
 import com.spotify.android.appremote.api.ContentApi;
@@ -24,6 +31,7 @@ import com.spotify.protocol.types.ListItem;
 import com.spotify.protocol.types.ListItems;
 
 import java.util.ArrayList;
+import java.util.function.Consumer;
 
 public class SpotifyMusicListActivity extends AppCompatActivity {
     DrawerLayout drawerLayout;
@@ -34,31 +42,29 @@ public class SpotifyMusicListActivity extends AppCompatActivity {
     ListView listView;
     AdapterView.OnItemClickListener navigationListener;
     AdapterView.OnItemClickListener playableListener;
+    SpotifyNavigationList navigationList;
+    RelativeLayout progressBar;
+    Boolean noMoreDataToFetch;
+    int startIndexOfDataFetch;
+    boolean userScrolled;
+    public static final int NUMBER_OF_ELEMENTS_TO_LOAD = 15;
     public static final String EXTRA_LIST_ITEM_SELECTED = "EXTRA_LIST_ITEM_SELECTED";
     static public ListItem lastSelected;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.spotify_bibliotheque_start);
-        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-        navigationView = (NavigationView) findViewById(R.id.nav_view);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        navigationView.bringToFront();
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open_drawer_description,
-                R.string.navigation_close_drawer_description);
-        drawerLayout.addDrawerListener(toggle);
-        toggle.syncState();
-        navigationView.setNavigationItemSelectedListener(new NavigationManager(this, this) {
-            @Override
-            public void gotoBibliothequeSpotify() {
-            }
-        });
-        navigationView.setCheckedItem(R.id.nav_spotify_bibliotheque);
-        NavigationManager.determinerOptionsAfficher(navigationView.getMenu());
+        progressBar = findViewById(R.id.loadItemsListView);
+        initializeNavigationElements();
         initializeOnClickListItemListeners();
-
         listView = (ListView) findViewById(R.id.list_spotify_bibliotheque_start);
+        initializeListViewContent();
+    }
+
+    private void initializeListViewContent(){
+        noMoreDataToFetch = false;
+        startIndexOfDataFetch = 0;
+        userScrolled = false;
         contenu = LierSpotifyActivity.appRemote.getContentApi();
         if(getIntent().getBooleanExtra(EXTRA_LIST_ITEM_SELECTED,false)) {
             getElementChildren(lastSelected);
@@ -76,6 +82,26 @@ public class SpotifyMusicListActivity extends AppCompatActivity {
             });
         }
     }
+
+    private void initializeNavigationElements(){
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationView = (NavigationView) findViewById(R.id.nav_view);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_open_drawer_description,
+                R.string.navigation_close_drawer_description);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(new NavigationManager(this, this) {
+            @Override
+            public void gotoBibliothequeSpotify() {
+            }
+        });
+        navigationView.setCheckedItem(R.id.nav_spotify_bibliotheque);
+        NavigationManager.determinerOptionsAfficher(navigationView.getMenu());
+    }
+
     @Override
     public void onBackPressed() {
         if (drawerLayout.isDrawerOpen(GravityCompat.START))
@@ -116,29 +142,72 @@ public class SpotifyMusicListActivity extends AppCompatActivity {
     }
 
     private void getElementChildren(ListItem item){
-        contenu.getChildrenOfItem(item,10,0).setResultCallback(new CallResult.ResultCallback<ListItems>() {
+        contenu.getChildrenOfItem(item,NUMBER_OF_ELEMENTS_TO_LOAD,startIndexOfDataFetch).setResultCallback(new CallResult.ResultCallback<ListItems>() {
             @Override
             public void onResult(ListItems listItems) {
                 ArrayList<SpotifyNavigationItem> navigationItems = new ArrayList<>();
                 for (ListItem item : listItems.items){
                     navigationItems.add(new SpotifyNavigationItem(item));
                 }
+                navigationList = new SpotifyNavigationList(navigationItems);
                 displayListItems(navigationItems);
             }
         });
     }
+    private void addElementChildren(ListItem item){
+        contenu.getChildrenOfItem(item,NUMBER_OF_ELEMENTS_TO_LOAD,startIndexOfDataFetch).setResultCallback(new CallResult.ResultCallback<ListItems>() {
+            @Override
+            public void onResult(ListItems listItems) {
+                if(listItems.items.length > 0) {
+                    ArrayList<SpotifyNavigationItem> items = new ArrayList<>();
+                    for (ListItem item : listItems.items) {
+                        items.add(new SpotifyNavigationItem(item));
+                    }
+                    navigationList.updateContentInList(items, progressBar);
+                }else {
+                    progressBar.setVisibility(View.GONE);
+                    noMoreDataToFetch = true;
+                }
+            }
+        });
+    }
     private void displayListItems(ArrayList<SpotifyNavigationItem> navigationItems){
-        SpotifyNavigationList navigationList = new SpotifyNavigationList(navigationItems);
         navigationList.transformToXML(listView,this);
         if(!navigationList.navigationItems.get(0).isPlayable())
             navigationList.setListOnClickListener(navigationListener,listView);
         else
             navigationList.setListOnClickListener(playableListener,listView);
+        navigationList.setListOnScrollListener(getLoadElementsAtEndOfListScrollListener(),listView);
     }
     private void reloadActivity(Boolean extraLastSelected){
         Intent intent = new Intent(this,SpotifyMusicListActivity.class);
         intent.putExtra(EXTRA_LIST_ITEM_SELECTED,extraLastSelected);
         startActivity(intent);
+    }
+    private void loadMoreContent(ListItem item){
+        if(!noMoreDataToFetch){
+            startIndexOfDataFetch += NUMBER_OF_ELEMENTS_TO_LOAD;
+            progressBar.setVisibility(View.VISIBLE);
+            addElementChildren(item);
+        }
+    }
+    public AbsListView.OnScrollListener getLoadElementsAtEndOfListScrollListener(){
+        return new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView arg0, int scrollState) {
+                if(scrollState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL)
+                    userScrolled = true;
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if(userScrolled && firstVisibleItem + visibleItemCount == totalItemCount) {
+                    userScrolled = false;
+                    loadMoreContent(lastSelected);
+                }
+            }
+        };
     }
 }
 class SpotifyNavigationItem{
@@ -170,19 +239,34 @@ class SpotifyNavigationItem{
     }
 }
 class SpotifyNavigationList{
+    ArrayAdapter<SpotifyNavigationItem> arrayAdapter;
     ArrayList<SpotifyNavigationItem> navigationItems;
     public SpotifyNavigationList(ArrayList<SpotifyNavigationItem> items){
         navigationItems = items;
     }
-
+    public void addItem(SpotifyNavigationItem item){
+        navigationItems.add(item);
+    }
     public void transformToXML(ListView listView,Context context){
-        ArrayAdapter<SpotifyNavigationItem> arrayAdapter = new ArrayAdapter<>(context,android.R.layout.simple_list_item_1,navigationItems);
+        arrayAdapter = new ArrayAdapter<>(context,android.R.layout.simple_list_item_1,navigationItems);
         listView.setAdapter(arrayAdapter);
+    }
+    public void updateContentInList(ArrayList<SpotifyNavigationItem> items, RelativeLayout progressBar){
+      //  new Handler().postDelayed(new Runnable() {
+          //  @Override
+         //   public void run() {
+                navigationItems.addAll(items);
+                arrayAdapter.notifyDataSetChanged();
+                progressBar.setVisibility(View.GONE);
+         //   }
+        //},5000);
     }
     public void setListOnClickListener(AdapterView.OnItemClickListener clickListener, ListView listView){
         listView.setOnItemClickListener(clickListener);
     }
-    //TODO Faire un principe de loading progressif avec le scroll down pour afficher plus de 10 résultats.
+    public void setListOnScrollListener(AbsListView.OnScrollListener scrollListener, ListView listView){
+        listView.setOnScrollListener(scrollListener);
+    }
     // TODO Permettre le visionnement du contenu jouable mais ayant des enfants
     // TODO actualiser l'affichage pour prendre le même que la bibliothèque locale.
 }
