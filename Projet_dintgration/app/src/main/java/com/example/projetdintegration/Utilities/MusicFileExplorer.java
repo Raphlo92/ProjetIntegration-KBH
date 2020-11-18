00e6d147cb01469f22854e7bd4a7f4efdc66bfaf
Wazarel_Namara <55587042+Raphlo92@users.noreply.github.com>
2020-11-18 13:47:28 -0500

    Changed the logo

--- Projet_dintgration/app/src/main/java/com/example/projetdintegration/Utilities/MusicFileExplorer.java
+++ Projet_dintgration/app/src/main/java/com/example/projetdintegration/Utilities/MusicFileExplorer.java
@@ -10,10 +10,12 @@ import java.util.Date;
 public class MusicFileExplorer {
     //PATH TO MUSICS : /storage/emulated/0/Music/{Artist}/{Album}/{Music}
     //PATH TO ALBUM PROPS : /storage/emulated/0/Music/{Artist}/{Album}/{PropFile}
+    //FILES TO IGNORE : files starting with '.'
 
     //FileOutputStream
     //FileInputStream
     //File
+
     static ArrayList<File> files = new ArrayList<>();
     //public static final String DIRECTORY_MUSIC = "src/main/res/raw";//"C:/Users/Kevin/Music";//"/storage/emulated/0/Music";
     public static final String DIRECTORY_MUSIC = "/storage/emulated/0/Music";
@@ -110,8 +112,15 @@ public class MusicFileExplorer {
         }
 
         Log.d(TAG, "getMimeType: extension = "+ extension);
+        String mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
 
-        return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+        if(extension != null){
+            if(mimeType != null){
+                return MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension);
+            }
+            return "unsupported";
+        }
+        return null;
     }
 
     public static File getFile(String path) {
